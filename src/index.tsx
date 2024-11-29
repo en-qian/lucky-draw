d568fe5252e114ccaae0d92fb12a19111c0aa7f5
enqian <eqreguse@gmail.com>
2024-11-29 12:39:28 +0800

    Prettier update

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
