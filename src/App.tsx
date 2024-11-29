bacfc6b6f933337c374a8cdfe087394c2fe36fc3
enqian <eqreguse@gmail.com>
2024-11-29 12:38:41 +0800

    Import component and page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '@styles/global.scss';
+import NotFound from '@pages/404';
+import { Route, Routes } from 'react-router-dom';
+import LuckyDraw from '@components/LuckyDraw';
 
 function App() {
   return (
     <>
       <ToastContainer position={'top-center'} autoClose={1500} />
+      <Routes>
+        <Route path="/" element={<LuckyDraw />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </>
   );
 }
