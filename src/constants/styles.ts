08a956da30a4bc1170f04958397f8d1e32ab85b9
enqian <eqreguse@gmail.com>
2024-11-29 12:36:25 +0800

    Add style constants

---
+++ src/constants/styles.ts
@@ -0,0 +1,36 @@
+export const Styles = {
+  h1FontSize: 'var(--h1-font-size)',
+  h1FontWeight: 'var(--h1-font-weight)',
+  h1LineHeight: 'var(--h1-line-height)',
+
+  h2FontSize: 'var(--h2-font-size)',
+  h2FontWeight: 'var(--h2-font-weight)',
+  h2LineHeight: 'var(--h2-line-height)',
+
+  h3FontSize: 'var(--h3-font-size)',
+  h3FontWeight: 'var(--h3-font-weight)',
+  h3LineHeight: 'var(--h3-line-height)',
+
+  p1FontSize: 'var(--p1-font-size)',
+  p1FontWeight: 'var(--p1-font-weight)',
+  p1LineHeight: 'var(--p1-line-height)',
+
+  buttonFontSize: 'var(--button-font-size)',
+  buttonFontWeight: 'var(--button-font-weight)',
+  buttonLineHeight: 'var(--button-line-height)',
+
+  black: 'var(--black)',
+  white: 'var(--white)',
+  grey: 'var(--grey)',
+  lightGrey: 'var(--lightGrey)',
+  lightPurple: 'var(--lightPurple)',
+  red: 'var(--red)',
+  lightRed: 'var(--lightRed)',
+  orange: 'var(--orange)',
+  lightOrange: 'var(--lightOrange)',
+  darkGreen: 'var(--darkGreen)',
+  green: 'var(--green)',
+  lightGreen: 'var(--lightGreen)',
+
+  boxShadow: 'var(--boxShadow)',
+} as const;
